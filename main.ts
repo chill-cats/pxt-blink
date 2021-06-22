896bf7da0c58e4a173bd6f772f07854966ae4255
Lê Nguyễn Hoàng Nhân <48344270+LeNguyenHoangNhan@users.noreply.github.com>
2021-06-22 14:56:59 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-function blink () {
-	
+namespace blink {
+    export function blink(pin: DigitalPin, delay: number) {
+        pins.digitalWritePin(pin, 0)
+        control.setInterval(function () {
+            pins.digitalWritePin(pin, 1)
+        }, delay, control.IntervalMode.Timeout)
+    }
 }
